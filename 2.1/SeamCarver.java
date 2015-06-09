ac8e98f7f48256dcb2b427570c221f39acb9d8e4
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-06-09 06:38:43 -0800

    Update SeamCarver.java
    Passed all tests, but memory usage is too large compared to reference. 90/100

--- 2.1/SeamCarver.java
+++ 2.1/SeamCarver.java
@@ -3,8 +3,12 @@ import java.awt.Color;
 public class SeamCarver {
 
 	private int W, H; //width and height of picture
-	private Color[][] c; //colors of each pixel
-	private double[][] E; //energy for each pixels
+	private Color[][] c, cT; //colors of each pixel
+	private double[][] E, ET; //energy for each pixels
+	private double[][] Ea, EaT; // accumulated energy from start to this pixel
+	private int[][] os, osT; //offset (-1/0/1) of parent
+	//double[] Eal = new double[W]; //Ea on current line
+	//double[] tmp = new double[W]; //tmp array
 
 	// create a seam carver object based on the given picture
 	public SeamCarver(final Picture picture)  {  
@@ -13,15 +17,28 @@ public class SeamCarver {
 		H = picture.height();
 		c = new Color[H][W]; //row major
 		E = new double[H][W]; 		
+		Ea = new double[H][W];
+		os = new int[H][W];				
+
+		cT = new Color[W][H]; //transpose
+		ET = new double[W][H]; //transpose
+		EaT = new double[W][H]; //transpose
+		osT = new int[W][H]; //transpose
 
 		//first pass init color matrix
-		for (int ir = 0; ir < H; ++ir)
-			for (int jc = 0; jc < W; ++jc)
+		for (int ir = 0; ir < H; ++ir) {
+			for (int jc = 0; jc < W; ++jc) {
 				c[ir][jc] = picture.get(jc, ir);
+				cT[jc][ir] = c[ir][jc];
+			}
+		}
 		//need a second pass to calculate energy
-		for (int ir = 0; ir < H; ++ir)
-			for (int jc = 0; jc < W; ++jc)				
+		for (int ir = 0; ir < H; ++ir) {
+			for (int jc = 0; jc < W; ++jc) {				
 				E[ir][jc] = calcEnergy(ir, jc);
+				ET[jc][ir] = E[ir][jc];
+			}
+		}
 	}
 
 	// current picture
@@ -32,6 +49,7 @@ public class SeamCarver {
 				picture.set(jc, ir, c[ir][jc]);
 		return picture;
 	}
+
 	// width of current picture
 	public     int width() { return W; }        
 	// height of current picture
@@ -45,39 +63,43 @@ public class SeamCarver {
 
 	// sequence of indices for vertical seam
 	public   int[] findVerticalSeam() {
-		double[][] Ea; // accumulated energy from start to this pixel
-		int[][] os; //offset (-1/0/1) of parent	
-		Ea = new double[H][W];
-		os = new int[H][W];		
 
-		int[] seam = new int[H];
 		// Ea for the first row
-		for (int jc = 0; jc < W; ++jc) 
+		for (int jc = 0; jc < W; ++jc) { 
 			Ea[0][jc] = E[0][jc];
+			//Eal[jc] = E[0][jc];
+		}
 
 		for (int ir = 1; ir < H; ++ir) {
 			for (int jc = 0; jc < W; ++jc) {
 				int k = 0; //offset
 				if (jc > 0   && Ea[ir-1][jc-1] < Ea[ir-1][jc+k])
+					//if (jc > 0   && Eal[jc-1] < Eal[jc+k])
 					k = -1;
 				if (jc < W-1 && Ea[ir-1][jc+1] < Ea[ir-1][jc+k])
+					//if (jc < W-1 && Eal[jc+1] < Eal[jc+k])
 					k =  1;
 				os[ir][jc] = k;
 				Ea[ir][jc] = Ea[ir-1][jc+k] + E[ir][jc];
+				//tmp[jc] = Eal[jc+k] + E[ir][jc];
 			}
+			//Eal = tmp;
 		}
 
 		//find the pixel in the last row with minimum Ea
 		int jcMin = 0;
 		double EaMin = Ea[H-1][0];
+		//double EaMin = Eal[0];
 		for (int jc = 1; jc < W; ++jc)
 			if (Ea[H-1][jc] < EaMin) {
 				EaMin = Ea[H-1][jc];
+				//if (Eal[jc] < EaMin) {
+				//	EaMin = Eal[jc];
 				jcMin = jc;
 			}
 
 		//construct seam from minIdx
-		seam = new int[H];		
+		int[] seam = new int[H];		
 		seam[H-1] = jcMin;
 		int ir = H-1;
 		while (ir > 0) {
@@ -101,9 +123,37 @@ public class SeamCarver {
 			//shift pixels after seam line to left by 1
 			System.arraycopy(c[ir], j+1, c[ir], j, W-1-j);
 			System.arraycopy(E[ir], j+1, E[ir], j, W-1-j);
+			System.arraycopy(Ea[ir], j+1, Ea[ir], j, W-1-j);
+			System.arraycopy(os[ir], j+1, os[ir], j, W-1-j);								
 		}
+		//also update transpose
+		for (int ir = 0; ir < W-1; ++ir) {
+			for (int jc = 0; jc < H; ++jc) {				
+				cT[ir][jc] = c[jc][ir];
+				ET[ir][jc] = E[jc][ir];
+				EaT[ir][jc] = Ea[jc][ir];					
+				osT[ir][jc] = os[jc][ir];
+			}
+		}		
 		W--;
+		updateEnergy(seam);
+		check();
 	}
+	private void check() {
+		for (int ir = 0; ir < H; ++ir) {
+			for (int jc = 0; jc < W; ++jc) {
+				if (E[ir][jc] != ET[jc][ir])
+					throw new RuntimeException("Wrong E!");
+				if (Ea[ir][jc] != EaT[jc][ir])
+					throw new RuntimeException("Wrong Ea!");
+				if (c[ir][jc] != cT[jc][ir])
+					throw new RuntimeException("Wrong c!");
+				if (os[ir][jc] != osT[jc][ir])
+					throw new RuntimeException("Wrong os!");
+			}
+		}				
+	}
+
 	public   int[] findHorizontalSeam() {		
 		transpose();
 		int[] seam = findVerticalSeam();
@@ -113,28 +163,11 @@ public class SeamCarver {
 
 	// remove horizontal seam from current picture
 	public    void removeHorizontalSeam(int[] seam) {
-		if (seam == null) 
-			throw new NullPointerException();
-		if (H <= 1) 
-			throw new IllegalArgumentException("height <= 1");
-		if (!isLegalHSeam(seam)) 
-			throw new IllegalArgumentException("Illegal horizontal seam");
 		transpose();
 		removeVerticalSeam(seam);
 		transpose();
 	}
 
-	private boolean isLegalHSeam(int[] seam) {
-		if (seam.length != W) return false;
-		for (int w = 0; w < seam.length; ++w) {
-			//out of box
-			if (seam[w] < 0 || seam[w] >= H) return false;
-			//not adjacent
-			if (w > 0 && Math.abs(seam[w] - seam[w-1]) > 1) return false;
-			if (w < W-1 && Math.abs(seam[w] - seam[w+1]) > 1) return false;
-		}
-		return true;		
-	}
 	private boolean isLegalVSeam(int[] seam) {
 
 		if (seam.length != H) return false;
@@ -147,6 +180,18 @@ public class SeamCarver {
 		}
 		return true;		
 	}
+
+	//update energy near vertical seam
+	private void updateEnergy(int[] vseam) {
+		for (int ir = 0; ir < H; ++ir) {
+			int jc = vseam[ir];
+			if (jc > 0)
+				E[ir][jc-1] = ET[jc-1][ir] = calcEnergy(ir, jc-1);				
+			if (jc < W)
+				E[ir][jc] = ET[jc][ir] =  calcEnergy(ir, jc);
+		}
+	}
+
 	// calculate energy of x(irow), y(jcol)
 	private  double calcEnergy(int x, int y) { 
 		if (x == 0 || x == H-1 || y == 0 || y == W-1)
@@ -166,17 +211,19 @@ public class SeamCarver {
 	}			
 
 	private void transpose() {
-		Color[][] newc = new Color[W][H];	
-		double[][] newE = new double[W][H];
-		for (int ir = 0; ir < W; ++ir) {
-			for (int jc = 0; jc < H; ++jc) {
-				newc[ir][jc] = c[jc][ir];
-				newE[ir][jc] = E[jc][ir]; 	
-			}
-		}
-		c = newc;
-		E = newE;
+		//swap E and ET
+		double[][] t1 ;
+		t1 = E; E = ET; ET = t1;
+		t1 = Ea; Ea = EaT; EaT = t1;
+		int[][] t2;
+		t2 = os; os = osT; osT = t2;
+		//swap c and cT
+		Color[][] t3 = c;
+		c = cT;
+		cT = t3;		
 		//swap H and W
-		int tmp = H; H = W; W = tmp;		
+		int t4 = H; 
+		H = W; 
+		W = t4;		
 	}
 }
